2412ca914f7f7649ded005e700d3da50fff85222
gabriel-alves-p <gabrielmpalves@gmail.com>
2021-11-18 23:01:39 +0000

    add toggle button to below tablet screen width

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,41 @@ jQuery(document).ready(function($) {
     //Fire it when the page first loads:
     alterClass();
   });
+
+  jQuery(document).ready(function($) {
+    var alterClass2 = function() {
+      var ww = document.body.clientWidth;
+      if (ww < 768) {
+        $('.header').addClass('hide');
+      } else if (ww >= 768) {
+        $('.header').removeClass('hide');
+      };
+    };
+    $(window).resize(function(){
+      alterClass2();
+    });
+    //Fire it when the page first loads:
+    alterClass2();
+  });
+
+  jQuery(document).ready(function($) {
+    var alterClass3 = function() {
+      var ww = document.body.clientWidth;
+      if (ww < 768) {
+        $('.navigation-mobile').removeClass('hide');
+      } else if (ww >= 768) {
+        $('.navigation-mobile').addClass('hide');
+      };
+    };
+    $(window).resize(function(){
+      alterClass3();
+    });
+    //Fire it when the page first loads:
+    alterClass3();
+  });
+
+  $(document).ready(function(){
+    $('#icon').click(function(){
+        $('ul').toggleClass('show');
+    })
+})
