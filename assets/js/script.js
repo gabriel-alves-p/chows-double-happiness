5a9e292456897489eccaa90a09b553e50daeccd3
gabriel-alves-p <gabrielmpalves@gmail.com>
2021-11-25 21:25:52 +0000

    add javascript and make toggle menu animate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#icon').click(function(){
+    $('.menu-btn').click(function(){
         $('ul').toggleClass('show');
     })
 })
@@ -8,4 +8,27 @@ $(document).ready(function(){
   $('.mobile-li').click(function(){
       $('ul').toggleClass('show');
   })
+})
+
+const menuBtn = document.querySelector('.menu-btn');
+const menuItems = document.querySelector('.mobile-ul');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false
+    }
+})
+
+menuItems.addEventListener('click', () => {
+    if (menuItems.classList.contains('show')) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false
+    }
 })
